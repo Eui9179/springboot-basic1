beef83e0055e9dc342ee29149f16078e5438e8bc
Eui9179 <outdark97@gmail.com>
2023-03-14 11:27:35 +0900

    Controller도 클래스일 뿐이다.

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -2,20 +2,28 @@ package com.ll.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-/**
- * @Controller
- * 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행시켜라
- */
+//@Controller
+//만약에 '/home/main' 이런 요청이 오면 아래 메서드를 실행시켜라
 @Controller
+@RequestMapping("/home")
 public class HomeController {
-    @GetMapping("/home/main")
+    private int count = 0;
+
+    @GetMapping("/main")
     // @ResponseBody
     // 아래 메서드를 실행한 후 그 리턴값을 Response Body에 넣어줘
     @ResponseBody
     public String showMain() {
         return "안녕하세요";
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public Integer increaseCount() {
+        return ++count;
     }
 }
