9226cc5763985990e8af7c6ad6e438b4f73a8df8
Eui9179 <outdark97@gmail.com>
2023-03-17 11:11:11 +0900

    entity에 LocalDateTime 추가

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -2,11 +2,17 @@ package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime modifyDate;
 
     @Column(nullable = false)
     private String title;
